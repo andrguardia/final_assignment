a103550a6d337d26ea41b5af3671c6211b638e0a
Andre Guardia <andre.guardiaq@gmail.com>
2023-04-14 17:20:06 -0500

    Initial Commit

---
+++ final_assignment/final_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_assignmentApp.swift
+//  final_assignment
+//
+//  Created by IIT Phys 440 on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct final_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
