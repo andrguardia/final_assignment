eb14ef9ed0f752ffd9b878d6632742c4ecc9b960
Andre Guardia <andre.guardiaq@gmail.com>
2023-04-14 19:59:53 -0500

    The front end is fully functional now he he he. I also added all the different structures as well as their pseudo potential form factors. Now I need to compute the hamiltonian using matrix diagonalization. I will start doing this for the Silicon structure since it does not have an imaginary component. At this point i'm approximately 1/3 of the way there, but I am going to hustle my way thru setting up the hamiltonian for the Silicon structure after dinner (its 8pm and every store boutta close soon). After dinner Imma come back and get this bread LETSGO

--- final_assignment/ContentView.swift
+++ final_assignment/ContentView.swift
@@ -7,13 +7,54 @@
 
 import SwiftUI
 
+struct Structure: Hashable {
+    let name: String
+    let latticeVariable: Double
+    let pseudopotentialFormFactor_V3S: Double
+    let pseudopotentialFormFactor_V8S: Double
+    let pseudopotentialFormFactor_V11S: Double
+    let pseudopotentialFormFactor_V3A: Double
+    let pseudopotentialFormFactor_V4A: Double
+    let pseudopotentialFormFactor_V11A: Double
+}
+
 struct ContentView: View {
-    @State private var selectedStructure = ("", 0.0)
-    let structures = [("Si", 5.43), ("Other", 0.0)]
+    @State private var selectedStructure = Structure(name: "None Selected", latticeVariable: 0.00, pseudopotentialFormFactor_V3S:0.00, pseudopotentialFormFactor_V8S: 0.00, pseudopotentialFormFactor_V11S: 0.00, pseudopotentialFormFactor_V3A: 0.00, pseudopotentialFormFactor_V4A:0.00, pseudopotentialFormFactor_V11A: 0.00  )
+    
+    let structures = [
+        
+        Structure(name: "Si", latticeVariable: 5.43, pseudopotentialFormFactor_V3S:-0.21, pseudopotentialFormFactor_V8S: 0.04, pseudopotentialFormFactor_V11S: 0.08, pseudopotentialFormFactor_V3A: 0.00, pseudopotentialFormFactor_V4A:0.00, pseudopotentialFormFactor_V11A: 0.00  ),
+        
+        Structure(name: "Ge", latticeVariable: 5.66, pseudopotentialFormFactor_V3S:-0.23, pseudopotentialFormFactor_V8S: 0.01, pseudopotentialFormFactor_V11S: 0.06, pseudopotentialFormFactor_V3A: 0.00, pseudopotentialFormFactor_V4A:0.00, pseudopotentialFormFactor_V11A: 0.00  ),
+        
+        Structure(name: "Sn", latticeVariable: 6.49, pseudopotentialFormFactor_V3S:-0.20, pseudopotentialFormFactor_V8S: 0.00, pseudopotentialFormFactor_V11S: 0.04, pseudopotentialFormFactor_V3A: 0.00, pseudopotentialFormFactor_V4A:0.00, pseudopotentialFormFactor_V11A: 0.00  ),
+        
+        Structure(name: "GaP", latticeVariable: 5.44, pseudopotentialFormFactor_V3S:-0.22, pseudopotentialFormFactor_V8S: 0.03, pseudopotentialFormFactor_V11S: 0.07, pseudopotentialFormFactor_V3A: 0.12, pseudopotentialFormFactor_V4A:0.07, pseudopotentialFormFactor_V11A: 0.02  ),
+        
+        Structure(name: "GaAs", latticeVariable: 5.64, pseudopotentialFormFactor_V3S:-0.23, pseudopotentialFormFactor_V8S: 0.01, pseudopotentialFormFactor_V11S: 0.06, pseudopotentialFormFactor_V3A: 0.07, pseudopotentialFormFactor_V4A:0.05, pseudopotentialFormFactor_V11A: 0.01  ),
+        
+        Structure(name: "AlSb", latticeVariable: 6.13, pseudopotentialFormFactor_V3S:-0.21, pseudopotentialFormFactor_V8S: 0.02, pseudopotentialFormFactor_V11S: 0.06, pseudopotentialFormFactor_V3A: 0.06, pseudopotentialFormFactor_V4A:0.04, pseudopotentialFormFactor_V11A: 0.02  ),
+
+        Structure(name: "InP", latticeVariable: 5.86, pseudopotentialFormFactor_V3S:-0.23, pseudopotentialFormFactor_V8S: 0.01, pseudopotentialFormFactor_V11S: 0.06, pseudopotentialFormFactor_V3A: 0.07, pseudopotentialFormFactor_V4A:0.05, pseudopotentialFormFactor_V11A: 0.01  ),
+        
+        Structure(name: "GaSb", latticeVariable: 6.12, pseudopotentialFormFactor_V3S:-0.22, pseudopotentialFormFactor_V8S: 0.00, pseudopotentialFormFactor_V11S: 0.05, pseudopotentialFormFactor_V3A: 0.06, pseudopotentialFormFactor_V4A:0.05, pseudopotentialFormFactor_V11A: 0.01  ),
+        
+        Structure(name: "InAs", latticeVariable: 6.04, pseudopotentialFormFactor_V3S:-0.22, pseudopotentialFormFactor_V8S: 0.00, pseudopotentialFormFactor_V11S: 0.05, pseudopotentialFormFactor_V3A: 0.08, pseudopotentialFormFactor_V4A:0.05, pseudopotentialFormFactor_V11A: 0.03  ),
+        
+        Structure(name: "InSb", latticeVariable: 6.48, pseudopotentialFormFactor_V3S:-0.20, pseudopotentialFormFactor_V8S: 0.00, pseudopotentialFormFactor_V11S: 0.04, pseudopotentialFormFactor_V3A: 0.06, pseudopotentialFormFactor_V4A:0.05, pseudopotentialFormFactor_V11A: 0.01  ),
+        
+        Structure(name: "ZnS", latticeVariable: 5.41, pseudopotentialFormFactor_V3S:-0.22, pseudopotentialFormFactor_V8S: 0.03, pseudopotentialFormFactor_V11S: 0.07, pseudopotentialFormFactor_V3A: 0.24, pseudopotentialFormFactor_V4A:0.14, pseudopotentialFormFactor_V11A: 0.04  ),
+        
+        Structure(name: "ZnSe", latticeVariable: 5.65, pseudopotentialFormFactor_V3S:-0.23, pseudopotentialFormFactor_V8S: 0.01, pseudopotentialFormFactor_V11S: 0.06, pseudopotentialFormFactor_V3A: 0.18, pseudopotentialFormFactor_V4A:0.12, pseudopotentialFormFactor_V11A: 0.03  ),
+        
+        Structure(name: "ZnTe", latticeVariable: 6.07, pseudopotentialFormFactor_V3S:-0.22, pseudopotentialFormFactor_V8S: 0.00, pseudopotentialFormFactor_V11S: 0.05, pseudopotentialFormFactor_V3A: 0.13, pseudopotentialFormFactor_V4A:0.10, pseudopotentialFormFactor_V11A: 0.01  ),
+
+        Structure(name: "CdTe", latticeVariable: 6.41, pseudopotentialFormFactor_V3S:-0.20, pseudopotentialFormFactor_V8S: 0.00, pseudopotentialFormFactor_V11S: 0.04, pseudopotentialFormFactor_V3A: 0.15, pseudopotentialFormFactor_V4A:0.09, pseudopotentialFormFactor_V11A: 0.04  ),
+    ]
     
     var body: some View {
         
-        VStack(alignment: .leading) {
+        VStack(alignment: .center) {
             
             Text("Band Structure Calculator")
                 .font(.title)
@@ -25,52 +66,112 @@ struct ContentView: View {
                 .padding(.leading, 50)
                 .padding(.trailing, 50)
             
+            Divider()
+            
             Text("Select Material")
                 .font(.title2)
                 .fontWeight(.bold)
                 .padding(.all, 25)
             
-            HStack{
-                Text("To use this calculator, please select a semiconductor structure from the dropdown menu:")
-                    .padding(.leading, 50)
-                    .padding(.trailing, 50)
-                
-                Picker(selection: $selectedStructure , label: Text("")) {
-                    ForEach(0..<structures.count) { i in
-                        Text(self.structures[i].0)
-                    }
+            Text("Please select a semiconductor structure from the dropdown menu:")
+                .padding(.leading, 50)
+                .padding(.trailing, 50)
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            Picker(selection: $selectedStructure, label: Text("")) {
+                ForEach(structures, id: \.self) { structure in
+                    Text(structure.name)
                 }
-                .pickerStyle(MenuPickerStyle())
-                .frame(width: 100)
+            }
+            .pickerStyle(MenuPickerStyle())
+            .frame(width: 100)
+            .frame(maxWidth: .infinity, alignment: .center)
+            .padding(.top, 5)
+
+            Text("Band Structure: \(selectedStructure.name)")
+                .multilineTextAlignment(.leading)
+                .padding(.leading, 50)
+                .padding(.trailing, 50)
+                .padding(.top, 5)
+            
+            Text("Lattice Variable [Å]: \(selectedStructure.latticeVariable)")
+                .multilineTextAlignment(.leading)
+                .padding(.leading, 50)
+                .padding(.trailing, 50)
+                .padding(.top, 5)
+            
+            HStack{
+                VStack{
+                    Text("Pseudopotential Form Factor V3S: \(selectedStructure.pseudopotentialFormFactor_V3S)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 50)
+                        .padding(.trailing, 50)
+                        .padding(.top, 5)
+                    
+                    Text("Pseudopotential Form Factor V8S: \(selectedStructure.pseudopotentialFormFactor_V8S)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 50)
+                        .padding(.trailing, 50)
+                        .padding(.top, 5)
+                    
+                    Text("Pseudopotential Form Factor V11S: \(selectedStructure.pseudopotentialFormFactor_V11S)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 50)
+                        .padding(.trailing, 50)
+                        .padding(.top, 5)
+                }.padding(.top, 25)
+                VStack{
+                    Text("Pseudopotential Form Factor V3A: \(selectedStructure.pseudopotentialFormFactor_V3A)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 50)
+                        .padding(.trailing, 50)
+                        .padding(.top, 5)
+                    
+                    Text("Pseudopotential Form Factor V4A: \(selectedStructure.pseudopotentialFormFactor_V4A)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 50)
+                        .padding(.trailing, 50)
+                        .padding(.top, 5)
+                    
+                    Text("Pseudopotential Form Factor V11A: \(selectedStructure.pseudopotentialFormFactor_V11A)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 50)
+                        .padding(.trailing, 50)
+                        .padding(.top, 5)
+                }.padding(.top, 25)
             }
             
-            Divider()
             
-            Text("Results")
-                .font(.title2)
-                .fontWeight(.bold)
-                .padding(.all, 25)
             
-            Text("Band Structure:" + String(selectedStructure.0))
-                .multilineTextAlignment(.leading)
-                .padding(.leading, 50)
+            HStack{
+                Button(action: {calculate()}) {
+                    Text("Calculate")
+                }
                 .padding(.trailing, 50)
-            Text("Lattice Variable (a) in Angstroms:" + String(selectedStructure.1))
-                .multilineTextAlignment(.leading)
-                .padding(.leading, 50)
-                .padding(.trailing, 50)
+                
+                Button(action: {clear()}) {
+                    Text("Clear")
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+            .padding(.top, 25)
+            
+
             
         }
+    }
+    
+    func calculate(){
+        print("Plot button works lmao")
+    }
+
+    func clear(){
+        print("Clear button works lmao")
     }
 }
 
 
 
-
-
-func calculate(){
-    
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
